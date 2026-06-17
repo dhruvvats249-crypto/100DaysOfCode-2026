56e3136b6932f20d5d91a5f17d6d9a18b23e276e
dhruvvats249-crypto <dhruvvats249@gmail.com>
2026-06-17 20:34:26 +0530

    Create Q2.py

---
+++ DSA/DhruvVats_590011246/Day_3/Q2.py
@@ -0,0 +1,44 @@
+class Solution:
+    def countFreq(self, arr, target):
+        def first_occurrence():
+            low, high = 0, len(arr) - 1
+            ans = -1
+
+            while low <= high:
+                mid = (low + high) // 2
+
+                if arr[mid] == target:
+                    ans = mid
+                    high = mid - 1
+                elif arr[mid] < target:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+
+            return ans
+
+        def last_occurrence():
+            low, high = 0, len(arr) - 1
+            ans = -1
+
+            while low <= high:
+                mid = (low + high) // 2
+
+                if arr[mid] == target:
+                    ans = mid
+                    low = mid + 1
+                elif arr[mid] < target:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+
+            return ans
+
+        first = first_occurrence()
+
+        if first == -1:
+            return 0
+
+        last = last_occurrence()
+
+        return last - first + 1
